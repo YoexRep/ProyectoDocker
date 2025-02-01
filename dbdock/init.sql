6b6eba1515dcadbc1e6060bc292408fcdf0ddfd1
yoex <ytentertainemnt94@gmail.com>
2025-02-01 15:40:50 -0400

    se agrego workflow para bd

--- dbdock/init.sql
+++ dbdock/init.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS t_saludos (
 );
 
 INSERT INTO t_saludos (f_saludo) VALUES
-('Hola primer saludo'),
-('Hola segundo saludo'),
-('Hola tercer saludo');
+('Hola primer saludo Actualizado workflow'),
+('Hola segundo saludo Actualizado workflow'),
+('Hola tercer saludo Actualizado workflow');
